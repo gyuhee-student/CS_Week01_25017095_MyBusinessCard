b5d27b7407d31d9fae26e8037e5aeb608f13a070
황규희 <gyuhee3021@gmail.com>
2026-03-08 16:21:29 +0900

    1주차 실습 완료

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -42,21 +48,106 @@
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            pictureBox1.MouseEnter += pictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += pictureBox1_MouseLeave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("한컴 고딕", 35.9999962F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(286, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 62);
+            label1.TabIndex = 1;
+            label1.Text = "황규희";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label2.ForeColor = SystemColors.HotTrack;
+            label2.Location = new Point(286, 153);
+            label2.Name = "label2";
+            label2.Size = new Size(503, 80);
+            label2.TabIndex = 2;
+            label2.Text = "소속 : 수원대학교 지능형SW융합대학\r\n컴퓨터SW 2학년\r\n";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label3.ForeColor = Color.SaddleBrown;
+            label3.Location = new Point(286, 260);
+            label3.Name = "label3";
+            label3.Size = new Size(454, 40);
+            label3.TabIndex = 3;
+            label3.Text = "이메일 : gyuhee3021@naver.com";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button1.ForeColor = Color.DarkBlue;
+            button1.Location = new Point(578, 382);
+            button1.Name = "button1";
+            button1.Size = new Size(210, 56);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button3.ForeColor = Color.FromArgb(0, 0, 192);
+            button3.Location = new Point(286, 323);
+            button3.Name = "button3";
+            button3.Size = new Size(146, 46);
+            button3.TabIndex = 6;
+            button3.Text = "GitHub";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button4.Location = new Point(69, 382);
+            button4.Name = "button4";
+            button4.Size = new Size(153, 45);
+            button4.TabIndex = 7;
+            button4.Text = "사진 변경";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button3;
+        private Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,51 @@ namespace WinFormsApp3
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/gyuhee-student",
+                UseShellExecute = true
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.picture_for_homework;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.None;
+            pictureBox1.Refresh(); 
+        }
     }
 }
